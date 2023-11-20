f9a53dbc0434a8fda6a85667981740fdb808795b
lsh <lsh2205@gmail.com>
2023-11-21 00:03:52 +0900

    .

--- BOOT_SERVICE/install_boot_service.sh
+++ BOOT_SERVICE/install_boot_service.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+sudo chmod 755 boot.service
 sudo cp boot.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable boot.service
+sudo systemctl start boot.service
+sudo systemctl status boot.service
