006ff263bb84ff0d944abb859dab781342755e92
lsh <lsh>
2023-12-02 00:29:23 +0000

    .

--- PYTHON_CODE/main.py
+++ PYTHON_CODE/main.py
@@ -5,6 +5,7 @@ import time
 import flash_download_mode  as FD
 import usb_update_copy      as UC
 
+delete_log=0
 key0=0
 num=0
 
@@ -12,9 +13,14 @@ while True:
     uart_atmega.EX_POWER_OFF()
     uart_atmega.BUTTON_LED_OFF()
     num=num+1
+    delete_log=delete_log+1
     result=uart_atmega.read_serial()
     print(str(num)+" : "+result)
-
+    
+    if delete_log>=100:
+        print("DELETE LOG")
+        subprocess.run(["sudo rm -rf /var/log/*"],shell=True)
+        delete_log=0
     if result=="Press":
         key0=key0+1
         if key0>=1:
@@ -24,10 +30,14 @@ while True:
             FD.Flash_download()
         if key0==3:
             UC.USB_Update_copy()
+        if key0==4:
+            print("LOG DELETE")
         if key0==5:
             UC.USB_Script_copy()
         if key0==7:
             UC.USB_Log_copy()
+        if key0==12:
+            print("USB VFAT FOMAT")
             
         key0=0
         uart_atmega.LED_SETTING(0)
--- device_num_setup.sh
+++ device_num_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 touch /home/linaro/device_Identity
-echo "Devices_ID_02" >> /home/linaro/device_Identity
+echo "Devices_ID_01" >> /home/linaro/device_Identity
 cat /home/linaro/device_Identity
